dc1f933c6fbd6a0347399adb6558d4ff21fe2bcc
goldeneaglewarrior <bruntond23@gmail.com>
2021-06-13 12:24:51 +0100

    commit

--- r_scripts/map_boundaries.R
+++ r_scripts/map_boundaries.R
@@ -6,6 +6,7 @@ library(ggplot2)
 library(dplyr)
 #install.packages("ggsn")
 library(ggsn)
+library(spsurvey) # read.dbf
 
 getwd()
 
@@ -14,6 +15,16 @@ tan_dist <- st_read("data/tan_district/Districts.shp")
 tan_reg <- st_read("data/tan_regions/Regions.shp")
 
 drc_prov <- st_read("data/drc_province/COD_admbnda_adm1_20170407.shp")
+
+## polygon outline of tanzania -----
+tanza <-  map_data("world", regions = "Tanzania")
+
+
+lakes <- st_read("data/african_waterbodies/Africa_waterbody.shp")
+
+rivers <- st_read("data/african_rivers/rivers_africa_37333.shp")
+rivers <- read.dbf("african_rivers/rivers_africa_37333.dbf")
+
 
 
 tan_dist
@@ -46,4 +57,38 @@ drc_prov
 katanga <- drc_prov %>% 
   filter(NOM %in% c("Lualaba", "Lomami", "Haut-Lomami", "Haut-Katanga", "Tanganyika"))
 
+
+
+
+
+####
+## Lakes ----
+
+ggplot(lakes) +
+  geom_sf(colour = "black", fill = "azure") +
+  theme_classic() +
+  labs(caption = "Shape source from \ndatacatalog.worldbank.org") +
+  xlim(30, 40) +
+  ylim(-12, 0) +
+  geom_polygon(data = tanza, 
+               aes(x=long, y = lat, group = group), 
+               fill = NA, size = 1, colour = "black") +
+  coord_sf()
+
+
+## rivers
+
+
+ggplot(rivers) +
+  geom_sf(colour = "blue") +
+  theme_classic() +
+  labs(caption = "Shape source from \nfao.org/geonetwork") +
+  xlim(30, 40) +
+  ylim(-12, 0) +
+  geom_polygon(data = tanza, 
+               aes(x=long, y = lat, group = group), 
+               fill = NA, size = 2, colour = "black") 
+
+
+unique(rivers$SUB_NAME)
 
