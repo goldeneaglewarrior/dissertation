219b0a9a1783a949d5e11019d5ae04e504315b0b
goldeneaglewarrior <bruntond23@gmail.com>
2021-07-14 15:38:37 +0100

    cmt

--- r_scripts/tanz_small_cities.R
+++ r_scripts/tanz_small_cities.R
@@ -13,7 +13,7 @@ library(raster)
 library(rnaturalearth) # 1:50 M scale country outline
 library(wesanderson) # colours
 library(smoothr) #fill.hole
-library(scales) # palette visualise show_col
+library(scales) # palette visualise show_col()
 library(tidyr)
 #library(ggridges)
 library(mapview) # interactive geometry viewing
@@ -25,10 +25,10 @@ africa_08 <- raster("Z:/mcnicol_agc_layers/1km/mcnicol_AGC2008_1km.tif")
 africa_09 <- raster("Z:/mcnicol_agc_layers/1km/mcnicol_AGC2009_1km.tif")
 africa_10 <- raster("Z:/mcnicol_agc_layers/1km/mcnicol_AGC2010_1km.tif")
 
-world_pop <- raster('Z:/Worldpop/global_mosaics/ppp_2010_1km_Aggregated.tif')
+world_pop <- raster('Z:/Worldpop/global_mosaics/ppp_2007_1km_Aggregated.tif')
 
-#pal_cava <- wes_palette("Cavalcanti1", 21, type = "continuous")
-show_col(pal_cava)
+pal <- wes_palette("Zissou1", 21, type = "continuous")
+show_col(pal)
 
 
 ## SPDF of country ----
@@ -75,15 +75,12 @@ names(agc_stack) <- c("agc2007",
                       "agc2010")
 
 
-## Calculating population centres ----
-
-
+## Calculating 2 types of population centres ----
 pop_1500 <- pop %>% 
   rasterToContour(nlevels = 40) %>% 
   st_as_sf() %>% 
   dplyr::filter(level == "1500")
 length(pop_1500$geometry[[1]])
-
 
 pop_4000 <- pop %>% 
   rasterToContour(nlevels = 40) %>% 
@@ -97,80 +94,61 @@ mapview(pop_4000)
 # Arusha, Moshi, Zanzibar, Dar es salaam, Morogoro, 
 # Tanga, Dodoma, Mbeya, Kigoma, Kasulu, Mwanza, Musoma
 
+pop_4000_bfr_sf <- pop_4000 %>% 
+  st_buffer(dist = 0.8) %>% #buffer to mostly exclude the charcoal buffer of other towns
+  fill_holes(threshold = 1000000000000)
 
+pop_4000_in <- pop %>%
+  mask(pop_4000_bfr_sf)
 
-pop_3000_sf <- pop_3000 %>% 
-  st_buffer(dist = .8) %>% #buffer to exclude the charcoal buffer of other towns
-  fill_holes(threshold = 1000000000000)# %>% 
-  as_Spatial()
-  
-
-mapview(pop_3000_sf)
-
-pop_3000_on <- pop %>% 
-  crop(extent(pop_3000_sf)) %>% 
-  mask(pop_3000_sf)
-
-pop_1500_high <- pop_3000_on %>% 
+pop_1500_hi <- pop_4000_in %>% 
   rasterToContour(nlevels = 50) %>% 
   st_as_sf() %>% 
-  dplyr::filter(level == "1500")
+  dplyr::filter(level == "1500") # if error check this is an actual level
 
-length(pop_1500_high$geometry[[1]])
-
-pop_3000_out <- pop %>% 
-  mask(pop_3000_sf, inverse = T)
+pop_4000_out <- pop %>% 
+  mask(pop_4000_bfr_sf, inverse = T)
 
 
-pop_1500_out <- pop_3000_out %>% 
-  rasterToContour(nlevels = 40) %>% 
+pop_1500_lo <- pop_4000_out %>% 
+  rasterToContour(nlevels = 10) %>% 
   st_as_sf() %>% 
-  dplyr::filter(level == "1500") # check this is a level
+  dplyr::filter(level == "1500") # if error check this is an actual level
 
-low_1500_bfr_80 <- pop_1500_out %>% 
-  st_buffer(dist = .8) %>%  # approximately 20km
-  fill_holes(threshold = 1000000000000)# %>% 
-  as_Spatial() # turns sf to spdf
-mapview(low_1500_bfr_80)
+pop_low1500_bfr_sf <- pop_1500_lo %>% 
+  st_buffer(dist = .8) %>% 
+  fill_holes(threshold = 1000000000000)# 
 
+pop_hi1500_bfr_sf <- pop_1500_hi %>% 
+  st_buffer(dist = .8) %>% 
+  fill_holes(threshold = 1000000000000)# 
 
+## investigate ----
+st_area(pop_hi1500_bfr_sf) # calculate area for
+st_area(pop_low1500_bfr_sf)
 
-
-
-area <- st_area(pop_300_sf0) # calculate area 
-area2 <- st_area(low_1500_bfr_80)
-area
-area2
-  
-
-intersection <- st_intersection(pop_3000_sf$geometry, low_1500_bfr_80$geometry)
-
-area_intersection <- st_area(intersection)
-area_intersection
-plot(intersection)
-
-ggplot() +
-  geom_sf(pop_3000_sf, mapping = aes(fill = level),
-          fill = "#EBCC2A", col = NA, alpha = 0.5) +
-  geom_sf(low_1500_bfr_80, mapping = aes(fill = level), 
-          fill  = "#78B7C5", col = NA, alpha = 0.5) +
-  geom_sf(pop_1500, mapping = aes(fill = level), col = "#EE3700") +
-  geom_polygon(data = country_outline, 
-               aes(x=long, y = lat, group = group), 
-               fill = NA, colour = "black") +
-  guides( fill = FALSE) +
-  theme_classic()
-
-
+intersection <- st_intersection(pop_hi1500_bfr_sf$geometry, pop_low1500_bfr_sf$geometry)
+st_area(intersection)
 
 
 
 length(pop_1500$geometry[[1]])
-length(pop_1500_out$geometry[[1]])
-length(pop_1500_high$geometry[[1]])
-# There are 57 urban areas with 1500 people km-2
-# 23 of them are within 80 km spheres of large cities, over 4000 people km-2
-# 34 of them are outside of 80km sphere of larger cities
+length(pop_1500_lo$geometry[[1]])
+length(pop_1500_hi$geometry[[1]])
+# There are 53 urban contour areas with 1500 people km-2
+# 27 of them are within 80 km spheres of large cities, over 4000 people km-2
+# 26 of them are outside of 80km sphere of larger cities
+
+#mapview(pop_low1500_bfr_sf)
+#mapview(pop_hi1500_bfr_sf)
+
+poplow_overlap_mask <- st_difference(pop_low1500_bfr_sf, pop_hi1500_bfr_sf)
+st_area(poplow_overlap_mask)
+poplow_overlap_spdf <- poplow_overlap_mask %>% 
+  as_Spatial()
+
+whole_inside <- st_union(pop_low1500_bfr_sf, pop_hi1500_bfr_sf)
+
 
 
 # reproject data to longitude/latitude for plotting
@@ -179,22 +157,66 @@ mwi_pa_data <- st_transform(mwi_pa_data, 4326)
 # = st_transform(us_states, crs = 2163)
 
 
+# Doesn't work yet but important
+#pop_1500_low <- st_transform(pop_1500_low, 4326)
+# proj4string(pop_1500_low) <- CRS("+init=epsg:4326") 
 
 
 
+## buffer urban area ----
 
+# buffer for lower urban area
 
-bfr_20 <- pop_1500 %>% 
+bfr_20_lo <- pop_1500_lo %>% 
   st_buffer(dist = .2) %>%  # approximately 20km
   fill_holes(threshold = 1000000000000) %>% 
   as_Spatial()
 
-bfr_50 <- pop_1500 %>% 
+bfr_50_lo <- pop_1500_lo %>% 
   st_buffer(dist = .5) %>%  # approximately 50km
   fill_holes(threshold = 1000000000000) %>% 
   as_Spatial()
 
-bfr_80 <- pop_1500 %>% 
+bfr_80_lo <- pop_1500_lo %>% 
+  st_buffer(dist = .8) %>%  # approximately 20km
+  fill_holes(threshold = 1000000000000) %>% 
+  as_Spatial()
+
+
+# crop and df 
+# 20km low
+agc_20_lo <- agc_stack %>% 
+  mask(poplow_overlap_spdf) %>% 
+  mask(bfr_20_lo) %>% 
+  as.data.frame(xy = T) 
+
+# 50km low
+agc_50_lo <- agc_stack %>% 
+  mask(poplow_overlap_spdf) %>% 
+  mask(bfr_50_lo)  %>% 
+  mask(bfr_20_lo, inverse = T) %>% 
+  as.data.frame(xy = T)
+
+# 80km low
+agc_80_lo <- agc_stack %>% 
+  mask(poplow_overlap_spdf) %>%
+  mask(bfr_80_lo) %>% 
+  mask(bfr_50_lo, inverse = T) %>% 
+  as.data.frame(xy = T)
+
+
+## high urban cities 
+bfr_20_hi <- pop_1500_hi %>% 
+  st_buffer(dist = .2) %>%  # approximately 20km
+  fill_holes(threshold = 1000000000000) %>% 
+  as_Spatial()
+
+bfr_50_hi <- pop_1500_hi %>% 
+  st_buffer(dist = .5) %>%  # approximately 50km
+  fill_holes(threshold = 1000000000000) %>% 
+  as_Spatial()
+
+bfr_80_hi <- pop_1500_hi %>% 
   st_buffer(dist = .8) %>%  # approximately 20km
   fill_holes(threshold = 1000000000000) %>% 
   as_Spatial()
@@ -202,97 +224,67 @@ bfr_80 <- pop_1500 %>%
 
 # crop and df 
 # 20km
-agc_20 <- agc_stack %>% 
-  crop(extent(bfr_20)) %>% 
-  mask(bfr_20) %>% 
-  as.data.frame(xy = T) %>% 
-  na.omit()
-
-
-# 50km crop 
-agc_50 <- agc_stack %>% 
-  crop(extent(bfr_50)) %>% 
-  mask(bfr_50)  %>% 
-  crop(bfr_20) %>% 
-  mask(bfr_20, inverse = T) %>% 
-  as.data.frame(xy = T) %>% 
-  na.omit()
-
-
-
-# 80km
-agc_80_what <- agc_stack %>% 
-  crop(extent(bfr_80)) %>% 
-  mask(bfr_80) %>% 
-  crop(bfr_50) %>% 
-  mask(bfr_50, inverse = T)# %>% 
-  as.data.frame(xy = T) %>% 
-  na.omit()#
-
-tanbox <- as(extent(29.3399975929,40.31659, -11.7209380022, -0.95), 
-             'SpatialPolygons')
-
-crs(tanbox) <- crs(tanbox)
-
-# outside buffer 
-agc_country <- agc_stack %>% 
-  #crop(extent(bfr_80)) %>% 
-  #mask(bfr_80, inverse = T) %>% 
-  as.data.frame(xy = T) %>% 
-  na.omit()
-
-
-agc_country <- africa_07 %>% 
-  crop(extent(tanbox)) %>% 
-  mask(tanbox)
-
-
-
-agc_country_1 <- agc_country %>% 
-  crop(extent(bfr_80)) %>% 
-  mask(bfr_80, inverse = T)
-
-agc_country_bfr <- agc_country_1 %>% 
-  crop(country_SPDF) %>% 
-  mask(country_SPDF) %>% 
+agc_20_hi <- agc_stack %>% 
+  mask(bfr_20_hi) %>% 
   as.data.frame(xy = T)
 
+# 50km crop 
+agc_50_hi <- agc_stack %>% 
+  mask(bfr_50_hi)  %>% 
+  mask(bfr_20_hi, inverse = T) %>% 
+  as.data.frame(xy = T)
+
+# 80km
+agc_80_hi <- agc_stack %>% 
+  mask(bfr_80_hi) %>% 
+  mask(bfr_50_hi, inverse = T) %>% 
+  as.data.frame(xy = T)
+
+
+## outside 
+# rural buffer 
+agc_rural <- agc_stack %>% 
+  mask(whole_inside, inverse = T) %>% 
+  as.data.frame(xy = T)
+
+
 ggplot() +
-  geom_tile(agc_country_bfr, mapping = aes(x=x,y=y, fill = mcnicol_AGC2007_1km)) +
-  theme_classic() +
+  geom_tile(agc_rural, mapping = aes(x=x,y=y, fill= agc2007, col = "#78B7C5")) +
+  geom_tile(agc_20_lo,mapping = aes(x=x,y=y,fill = agc2007), col = "#EB5500") +
+  geom_tile(agc_50_lo,mapping = aes(x=x,y=y,fill = agc2007), col = "#E4BA10") +
+  geom_tile(agc_80_lo,mapping = aes(x=x,y=y,fill = agc2007), col = "#BDC367" ) +
+  geom_tile(agc_20_hi,mapping = aes(x=x,y=y,fill = agc2007), col = "#F21A00") + #dark red
+  geom_tile(agc_50_hi,mapping = aes(x=x,y=y,fill = agc2007), col = "#E1AF00") +
+  geom_tile(agc_80_hi,mapping = aes(x=x,y=y,fill = agc2007), col = "#EBCC2A" ) + #yellow
   geom_polygon(data = country_outline, 
                aes(x=long, y = lat, group = group), 
-               fill = NA, colour = "black")
+               fill = NA, colour = "black") +
+  coord_quickmap() +
+  theme(legend.position = "none") +
+  guides(color = FALSE, fill = FALSE) +
+  theme_classic()
 
 
-agc_80 <- africa_07 %>%
-  crop(extent(tanbox)) %>% 
-  mask(tanbox)
-agc_80 <- agc_80 %>% 
-  crop(extent(bfr_80)) %>% 
-  mask(bfr_80) #%>% 
-agc_80_in <- agc_80 %>% 
-  crop(extent(bfr_80)) %>% 
-  mask(bfr_50, inverse = T)# %>% 
-  as.data.frame(xy = T) %>% 
-  na.omit()#
 
-  
-agc_80_in_tan <- agc_80_in %>% 
-  #crop(extend(country_SPDF)) %>% 
-  mask(country_SPDF)
-  
-  
-plot(agc_80_what$agc2007)
-plot(agc_80_in_tan)
+#saving files to analyse in other scripts
+save(agc_20_hi,file="data/buffers/agc_20_hi")
+save(agc_50_hi,file="data/buffers/agc_50_hi")
+save(agc_80_hi,file="data/buffers/agc_80_hi")
+
+save(agc_20_lo,file="data/buffers/agc_20_lo")
+save(agc_50_lo,file="data/buffers/agc_50_lo")
+save(agc_80_lo,file="data/buffers/agc_80_lo")
+
+save(agc_rural,file="data/buffers/agc_rural")
 
 
-hlo <- africa_07 %>% 
-  
-  crop(extent(bfr_80)) %>% 
-  mask(bfr_80) %>% 
-  crop(bfr_50) %>% 
-  mask(bfr_50, inverse = T)
+save(bfr_20_hi, file = "data/buffers/bfr_20_hi")
+save(bfr_50_hi, file = "data/buffers/bfr_50_hi")
+save(bfr_80_hi, file = "data/buffers/bfr_80_hi")
 
-plot(agc_80_what$agc2007)
-plot(agc_80_in)
+save(bfr_20_lo, file = "data/buffers/bfr_20_lo")
+save(bfr_50_lo, file = "data/buffers/bfr_50_lo")
+save(bfr_80_lo, file = "data/buffers/bfr_80_lo")
+
+
+
